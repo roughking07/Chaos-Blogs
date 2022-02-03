f48a305bbf86358f61ccb7eea562f732e4cf7b97
Kunal Khatri <kunalkhatri017@gmail.com>
2022-02-03 14:45:59 +0530

    deploy on heroku

--- My_Blog_Project/settings.py
+++ My_Blog_Project/settings.py
@@ -130,10 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [STATIC_DIR,]
-STATIC_ROOT=os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Media
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
